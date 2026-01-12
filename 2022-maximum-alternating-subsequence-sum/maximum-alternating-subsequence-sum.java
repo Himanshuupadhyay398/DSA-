94f3f2fa734b9805d6953754a3947ed821322f74
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-01-12 15:42:00 +0530

    Time: 6 ms (91.71%) | Memory: 98 MB (88.32%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -1,22 +1,31 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
-        int n=nums.length;
-        long dp[][]=new long[n+1][2];
-        for(int i=0;i<nums.length;i++){
-            Arrays.fill(dp[i],-1);
+    //     int n=nums.length;
+    //     long dp[][]=new long[n+1][2];
+    //     for(int i=0;i<nums.length;i++){
+    //         Arrays.fill(dp[i],-1);
+    //     }
+    //     return recurse(0,nums,dp,true);
+    // }
+    // public long recurse(int index,int nums[],long dp[][],boolean visit){
+    //     if(index>=nums.length) return 0;
+    //     int flag=visit?1:0;
+    //     if((dp[index][flag])!=-1) return dp[index][flag];
+    //     long skip=recurse(index+1,nums,dp,visit);
+    //     long value=nums[index];
+    //     if(flag==0){
+    //         value=-value;
+    //     }
+    //     long take=value+recurse(index+1,nums,dp,!visit);
+    // return dp[index][flag]=Math.max(take,skip);
+        long even=0;
+        long odd=0;
+        for(int val:nums){
+            long newEven=Math.max(even,odd+val);
+            long newOdd=Math.max(odd,even-val);
+            even=newEven;
+            odd=newOdd;
         }
-        return recurse(0,nums,dp,true);
-    }
-    public long recurse(int index,int nums[],long dp[][],boolean visit){
-        if(index>=nums.length) return 0;
-        int flag=visit?1:0;
-        if((dp[index][flag])!=-1) return dp[index][flag];
-        long skip=recurse(index+1,nums,dp,visit);
-        long value=nums[index];
-        if(flag==0){
-            value=-value;
-        }
-        long take=value+recurse(index+1,nums,dp,!visit);
-    return dp[index][flag]=Math.max(take,skip);
+    return even;
     }
 }
