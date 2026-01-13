a6103e0437f36894995bfdd51d64cd99aee4bcb6
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-01-13 19:47:40 +0530

    Time: 493 ms (5.01%) | Memory: 237.2 MB (15.34%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -4,37 +4,37 @@ class Solution {
         //bottom up 
 
         int n = nums.length;
-        int []dp=new int[n];
-        Arrays.fill(dp,1);
-        int max=1;
-        for(int i=1;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[i]>nums[j]){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                }
-            }
-            max=Math.max(dp[i],max);
-        }
-    return max;
-    }
+    //     int []dp=new int[n];
+    //     Arrays.fill(dp,1);
+    //     int max=1;
+    //     for(int i=1;i<n;i++){
+    //         for(int j=0;j<i;j++){
+    //             if(nums[i]>nums[j]){
+    //                 dp[i]=Math.max(dp[i],dp[j]+1);
+    //             }
+    //         }
+    //         max=Math.max(dp[i],max);
+    //     }
+    // return max;
+    // }
     
     //memoization
 
-    //     dp = new int[n][n + 1];
-    //     for (int i = 0; i < n; i++) {
-    //         Arrays.fill(dp[i], -1);
-    //     }
-    //     dp[n][n+1]=0;
-    // }
-
-    // private int solve(int i, int prevIndex, int[] nums) {
-    //     if (i == nums.length) return 0;
-    //     if (dp[i][prevIndex + 1] != -1) return dp[i][prevIndex + 1];
-    //     int notTake = solve(i + 1, prevIndex, nums);
-    //     int take = 0;
-    //     if (prevIndex == -1 || nums[i] > nums[prevIndex]) {
-    //         take = 1 + solve(i + 1, i, nums);
-    //     }
-    //     return dp[i][prevIndex + 1] = Math.max(take, notTake);
-    // }
+        int [][]dp = new int[n+1][n+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],1);
+        }
+        
+        return recurse(0,nums,dp,-1);
+    }
+    public int recurse(int i,int nums[],int dp[][],int prev){
+        if(i>=nums.length) return 0;
+        if(dp[i][prev+1]!=1) return dp[i][prev+1];
+        int take=0;
+        if(prev==-1 || nums[i]>nums[prev]){
+            take=1+recurse(i+1,nums,dp,i);
+        }
+        int skip=recurse(i+1,nums,dp,prev);
+        return dp[i][prev+1]=Math.max(take,skip);
+    }
 }
