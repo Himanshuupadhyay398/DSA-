d698a3b0df3c2b75410abe8ea9a0d94471ab1973
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-01-14 01:57:08 +0530

    Time: 101 ms (14.79%) | Memory: 56 MB (9.20%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.java
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -1,34 +1,72 @@
 class Solution {
     public int longestStrChain(String[] words) {
-        Arrays.sort(words,(a,b)->a.length()-b.length());
+
+    // memoization
         int n=words.length;
-        int dp[]=new int[n];
-        Arrays.fill(dp,1);
-        int ans=0;
+        Arrays.sort(words,(a,b)->a.length()-b.length());
+        int dp[][]=new int[n+1][n+1];
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(predessors(words[j],words[i])){
-                    dp[i]=Math.max(dp[i],dp[j]+1);
-                }
-            }
-        ans=Math.max(ans,dp[i]);
+            Arrays.fill(dp[i],-1);
         }
-    return ans;
+        return recurse(words,dp,0,-1);
     }
-    public boolean predessors(String one,String two){
-        if(two.length()!=one.length()+1) return false;
-        boolean rep=false;
+    public int recurse(String words[],int dp[][],int index,int p){
+        if(index>=words.length) return 0;
+        if(dp[index][p+1]!=-1) return dp[index][p+1];
+
+        int take=0;
+        if(p==-1 || isPred(words[p],words[index])){
+            take=1+recurse(words,dp,index+1,index);
+        }
+        int skip=recurse(words,dp,index+1,p);
+        return dp[index][p+1]=Math.max(take,skip);
+    }
+    public boolean isPred(String one,String Two){
+        if(Two.length()!=one.length()+1) return false;
         int i=0,j=0;
-        while(i<one.length() && j<two.length()){
-            if(one.charAt(i)==two.charAt(j)){
+        boolean skipped=false;
+        while(i<one.length() && j<Two.length()){
+            if(one.charAt(i)==Two.charAt(j)){
                 i++;
                 j++;
             }else{
-                if(rep) return false;
-                rep=true;
+                if(skipped) return false;
+                skipped=true;
                 j++;
-            }
+            }     
         }
     return true;
     }
+
+    //Bottom up
+     //     Arrays.sort(words,(a,b)->a.length()-b.length());
+    //     int n=words.length;
+    //     int dp[]=new int[n];
+    //     Arrays.fill(dp,1);
+    //     int ans=0;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<i;j++){
+    //             if(predessors(words[j],words[i])){
+    //                 dp[i]=Math.max(dp[i],dp[j]+1);
+    //             }
+    //         }
+    //     ans=Math.max(ans,dp[i]);
+    //     }
+    // return ans;
+    // }
+    // public boolean predessors(String one,String two){
+    //     if(two.length()!=one.length()+1) return false;
+    //     boolean rep=false;
+    //     int i=0,j=0;
+    //     while(i<one.length() && j<two.length()){
+    //         if(one.charAt(i)==two.charAt(j)){
+    //             i++;
+    //             j++;
+    //         }else{
+    //             if(rep) return false;
+    //             rep=true;
+    //             j++;
+    //         }
+    //     }
+    // return true;
 }
