1f140a95ed01f807549fbcedee207e5bbb0d0538
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-02-03 22:58:19 +0530

    Time: 1 ms (72.27%) | Memory: 45 MB (10.08%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isTrionic(int[] nums) {
+        int n=nums.length;
+        int i=0;
+        while (i+1<n && nums[i]<nums[i+1]) {
+            i++;
+        }
+
+        if(i==n-1 || i==0) return false;
+
+        while(i+1<n && nums[i]>nums[i+1]){
+            i++;
+        }
+
+        if(i==n-1 || i==0) return false;
+
+        while(i+1<n){
+            if(nums[i]>=nums[i+1]) return false;
+            i++;
+        }
+    return true;
+
+    }
+}
