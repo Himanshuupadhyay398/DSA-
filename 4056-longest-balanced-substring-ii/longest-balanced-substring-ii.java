da16b5e30879fef4532d84cfe7cacc26ef904808
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-02-13 23:12:33 +0530

    Time: 71 ms (100.00%) | Memory: 101.9 MB (98.43%) - LeetSync

--- 4056-longest-balanced-substring-ii/longest-balanced-substring-ii.java
+++ 4056-longest-balanced-substring-ii/longest-balanced-substring-ii.java
@@ -1,52 +1,105 @@
 class Solution {
+     static class Pair {
+        int d1, d2;
+
+        Pair(int d1, int d2) {
+            this.d1 = d1;
+            this.d2 = d2;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Pair)) return false;
+            Pair p = (Pair) o;
+            return d1 == p.d1 && d2 == p.d2;
+        }
+
+        @Override
+        public int hashCode() {
+            return 31 * d1 + d2;
+        }
+    }
+
     public int longestBalanced(String s) {
+
         char[] c = s.toCharArray();
         int n = c.length;
-        
-        int cur_a = 0, cur_b = 0, cur_c = 0;        
-        int max_a = 0, max_b = 0, max_c = 0;
 
-        for (int i = 0; i < n; i++) {
-            if (c[i] == 'a') {
-                cur_a = (i > 0 && c[i-1] == 'a') ? cur_a + 1 : 1;
-                max_a = Math.max(max_a, cur_a);
-            } else if (c[i] == 'b') {
-                cur_b = (i > 0 && c[i-1] == 'b') ? cur_b + 1 : 1;
-                max_b = Math.max(max_b, cur_b);
-            } else { 
-                cur_c = (i > 0 && c[i-1] == 'c') ? cur_c + 1 : 1;
-                max_c = Math.max(max_c, cur_c);
+        int res = 0;
+
+        //Case-1
+        int cur = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (c[i] == c[i - 1]) {
+                cur++;
+            } else {
+                res = Math.max(res, cur);
+                cur = 1;
             }
         }
-        
-        int res = Math.max(Math.max(max_a, max_b), max_c);
-        
+        res = Math.max(res, cur);
+
+        //Case-2
         res = Math.max(res, find2(c, 'a', 'b'));
         res = Math.max(res, find2(c, 'a', 'c'));
         res = Math.max(res, find2(c, 'b', 'c'));
+
         
-        res = Math.max(res, find3(c));
-        
+        //Case-3
+        int ca = 0, cb = 0, cc = 0;
+
+        Map<Pair, Integer> mp = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+
+            if (c[i] == 'a') ca++;
+            else if (c[i] == 'b') cb++;
+            else cc++;
+
+            if(ca == cb && ca == cc)
+                res = Math.max(res, ca+cb+cc);
+
+            Pair key = new Pair(ca - cb, ca - cc);
+
+            Integer prev = mp.get(key);
+            if (prev != null) {
+                res = Math.max(res, i - prev);
+            } else {
+                mp.put(key, i);
+            }
+        }
+
         return res;
     }
-    
+
     private int find2(char[] c, char x, char y) {
-        int n = c.length, max_len = 0;
+
+        int n = c.length;
+        int max_len = 0;
+
         int[] first = new int[2 * n + 1];
         Arrays.fill(first, -2);
-        
-        int clear_idx = -1, diff = n;
+
+        int clear_idx = -1;
+        int diff = n;
+
         first[diff] = -1;
-        
+
         for (int i = 0; i < n; i++) {
+
             if (c[i] != x && c[i] != y) {
+
                 clear_idx = i;
                 diff = n;
                 first[diff] = clear_idx;
+
             } else {
+
                 if (c[i] == x) diff++;
                 else diff--;
-                
+
                 if (first[diff] < clear_idx) {
                     first[diff] = i;
                 } else {
@@ -54,29 +107,7 @@ class Solution {
                 }
             }
         }
-        
-        return max_len;
-    }
-    
-    private int find3(char[] c) {
-        long state = Long.MAX_VALUE / 2;
-        Map<Long, Integer> first = new HashMap<>();
-        first.put(state, -1);
-    
-        int max_len = 0;
-    
-        for (int i = 0; i < c.length; i++) {
-            if (c[i] == 'a') state += 1_000_001;
-            else if (c[i] == 'b') state -= 1_000_000;
-            else state--;
-        
-            if (first.containsKey(state)) {
-                max_len = Math.max(max_len, i - first.get(state));
-            } else {
-                first.put(state, i);
-            }
-        }
-    
+
         return max_len;
     }
 }
