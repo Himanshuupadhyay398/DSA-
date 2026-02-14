9b8a4998a995ff838667549614c0f665fba9baaa
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-02-14 15:58:26 +0530

    Time: 3 ms (76.33%) | Memory: 48.2 MB (76.32%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.java
@@ -0,0 +1,25 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double dp[][]=new double[query_row+1][query_row+1];
+        for(int i=0;i<=query_row;i++){
+            Arrays.fill(dp[i],-1.0);
+        }
+        return Math.min(1.0,recurse(poured,query_row,query_glass,dp));
+    }
+
+    public double recurse(int poured,int i,int j,double dp[][]){
+
+        if(i<j || i<0 || j<0) return 0.0;
+        if(i==0 && j==0) return poured;
+
+        if(dp[i][j]!=-1.0) return dp[i][j];
+
+        double upLeft=(recurse(poured,i-1,j-1,dp)-1)/2.0;
+        double upRight=(recurse(poured,i-1,j,dp)-1)/2.0;
+
+        if(upLeft<0) upLeft=0.0;
+        if(upRight<0) upRight=0.0;
+
+    return dp[i][j]=upLeft+upRight;
+    }
+}
