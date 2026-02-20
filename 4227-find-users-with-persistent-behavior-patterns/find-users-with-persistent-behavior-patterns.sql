e04d379bbf020a0f423abd57016b34725cce3abf
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-02-20 11:37:02 +0530

    Time: 601 ms (5.04%) | Memory: 0B (100.00%) - LeetSync

--- 4227-find-users-with-persistent-behavior-patterns/find-users-with-persistent-behavior-patterns.sql
+++ 4227-find-users-with-persistent-behavior-patterns/find-users-with-persistent-behavior-patterns.sql
@@ -1,23 +1,11 @@
 # Write your MySQL query statement below
-WITH one_action_per_day AS (
-	SELECT user_id, action_date, MAX(action) action
-	FROM activity
-	GROUP BY user_id, action_date
-	HAVING COUNT(1) = 1
-), find_consecutive_days AS (
-	SELECT user_id, action_date, action,
-    DATE_ADD(action_date, INTERVAL - row_number() over(partition by user_id order by action_date) DAY ) group_action_date
-	FROM one_action_per_day
-), filter_streak AS (
-	SELECT user_id, action, 
-	min(action_date) start_date,
-	max(action_date) end_date,
-	count(1) streak_length
-	FROM find_consecutive_days
-	GROUP BY user_id, action, group_action_date
-	HAVING COUNT(1) > 4
-)
-SELECT user_id, action, streak_length, start_date, end_date
-FROM filter_streak f
-WHERE streak_length >= (SELECT MAX(streak_length) FROM filter_streak f1 WHERE f.user_id = f1.user_id)
-ORDER BY streak_length DESC, user_id ASC
+# Write your MySQL query statement below
+select user_id, action, streak_length, start_date, end_date
+from (
+        select user_id, action, count(action) as streak_length, min(action_date) as start_date, max(action_date) as end_date, rank() over (partition by user_id order by count(action) desc) as ranks
+        from activity
+        group by user_id, action
+        having datediff(end_date, start_date)= streak_length - 1 and streak_length>=5
+        order by streak_length desc, user_id
+) as temp
+where ranks=1
