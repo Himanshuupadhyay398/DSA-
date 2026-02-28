9fecd73e04b241654289473e8c39f89aaa550c2a
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-02-28 16:07:28 +0530

    Time: 24 ms (98.84%) | Memory: 42.3 MB (84.88%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -1,21 +1,14 @@
 class Solution {
     public int concatenatedBinary(int n) {
         int mod=1000000007;
-        StringBuilder s=new StringBuilder();
+        int count=0;
+        long result=0;
         for(int i=1;i<=n;i++){
-            s.append(Integer.toBinaryString(i));
-        }
-        long answer=0;
-        long power=1;
-        for(int i=s.length()-1;i>=0;i--){
-            int pos=s.length()-1-i;
-            if(s.charAt(i)=='1' && i==s.length()-1){
-                answer++;
-            }else if(s.charAt(i)=='1'){
-                answer=(answer+power) % mod;
+            if ((i&(i - 1)) == 0){
+                count++;
             }
-            power=(2*power) % mod;
+            result=((result<<count)+i)%mod;
         }
-    return (int)answer;
+    return (int)result;
     }
 }
