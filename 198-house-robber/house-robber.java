b2152dc069ace40e3b8487560ed4509e42e889bf
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-03-18 12:42:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (70.06%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,29 +1,17 @@
 class Solution {
-    //memoization
-    // public int rob(int[] nums) {
-    //     int n=nums.length;
-    //     int dp[]=new int[n+1];
-    //     Arrays.fill(dp,-1);
-    //     return houseRob(0,nums,dp);
-    // }           
-    // public int houseRob(int index,int nums[],int[]dp){
-    //     if(index>=nums.length){
-    //         return 0;
-    //     }
-    //     if(dp[index]!=-1) return dp[index];
-
-    //     int pick=nums[index]+houseRob(index+2,nums,dp);
-    //     int notpick=houseRob(index+1,nums,dp);
-    //     return dp[index]=Math.max(pick,notpick);
-    // }       
+    int dp[]=new int[101];
     public int rob(int[] nums) {
-        int n=nums.length;
-        int dp[]=new int[n+1];
-        dp[0]=0;
-        dp[1]=nums[0];
-        for(int i=2;i<=n;i++){
-            dp[i]=Math.max(dp[i-1],nums[i-1]+dp[i-2]);
-        }
-    return dp[n];
+        Arrays.fill(dp,-1);
+        return recurse(nums,0);
     }
-} 
+    public int recurse(int nums[],int idx){
+        int n=nums.length;
+        if(idx>=n){
+            return 0;
+        }
+        if(dp[idx]!=-1) return dp[idx];
+        int pick=nums[idx]+recurse(nums,idx+2);
+        int notake=recurse(nums,idx+1);
+        return dp[idx]=Math.max(pick,notake);
+    }
+}
