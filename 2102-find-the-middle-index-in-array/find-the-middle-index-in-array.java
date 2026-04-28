733f8fe020821e44cd72341472769f82338484e3
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-04-28 11:05:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (40.08%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -1,24 +1,46 @@
+// class Solution {
+//     public int findMiddleIndex(int[] nums) {
+//         int n=nums.length;
+//         int  prefix[]=new int[n];
+//         int suffix[]=new int[n];
+//         prefix[0]=nums[0];
+//         for(int i=1;i<n;i++){
+//             prefix[i]=nums[i]+prefix[i-1];
+//         }
+//         suffix[n-1]=nums[n-1];
+//         for(int i=n-2;i>=0;i--){
+//             suffix[i]=suffix[i+1]+nums[i];
+//         }
+//         for(int i=0;i<n;i++){
+//             int left=(i>0)?prefix[i-1]:0;
+//             int right=(i<nums.length-1)?suffix[i+1]:0;
+
+//             if(left==right){
+//                 return i;
+//             }
+//         }
+//     return -1;
+//     }
+// } 
+
+
 class Solution {
     public int findMiddleIndex(int[] nums) {
-        int n=nums.length;
-        int  prefix[]=new int[n];
-        int suffix[]=new int[n];
-        prefix[0]=nums[0];
-        for(int i=1;i<n;i++){
-            prefix[i]=nums[i]+prefix[i-1];
+        if(nums.length==1){
+            return 0;
         }
-        suffix[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            suffix[i]=suffix[i+1]+nums[i];
+        int t = 0;
+        for(int i:nums){
+            t+=i;
         }
-        for(int i=0;i<n;i++){
-            int left=(i>0)?prefix[i-1]:0;
-            int right=(i<nums.length-1)?suffix[i+1]:0;
-
-            if(left==right){
+        int left =0;
+        for(int i=0;i<nums.length;i++){
+            int right = t - nums[i] - left;
+            if(right==left){
                 return i;
             }
+            left +=nums[i];
         }
     return -1;
     }
-} 
+}
