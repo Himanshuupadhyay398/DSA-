6ce8487694779dc0528f1da7225c103077f8509b
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-06-17 19:53:17 +0530

    Time: 12 ms (93.13%) | Memory: 46.1 MB (91.73%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,25 +1,13 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
     int dp[]=new int[amount+1];
-    Arrays.fill(dp,-1);
-    int answer=recurse(coins,amount,dp);
-    return answer==Integer.MAX_VALUE?-1:answer;
-    }
-    public int recurse(int coins[],int amount,int dp[]){
-        if(amount==0) return 0;
-        if(amount<0) return Integer.MAX_VALUE;
-
-        if(dp[amount]!=-1) return dp[amount];
-
-       int min = Integer.MAX_VALUE;
-
-        for (int coin : coins) {
-            int res = recurse(coins, amount - coin, dp);
-            if (res != Integer.MAX_VALUE) {
-                min = Math.min(min, 1 + res);
+    Arrays.fill(dp,amount+1);
+    dp[0]=0;
+    for(int coin:coins){
+        for(int i=coin;i<dp.length;i++){
+            dp[i]=Math.min(dp[i],dp[i-coin]+1); 
             }
         }
-
-        return dp[amount] = min;
+    return dp[amount]>amount?-1:dp[amount];
     }
 }
