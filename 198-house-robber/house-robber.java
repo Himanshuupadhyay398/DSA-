85da7c02439640ddddc4fff4ced18afb06640800
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-07-16 17:11:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.05%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
+        if(n==1) return nums[0];
         int dp[]=new int[n];
         Arrays.fill(dp,-1);
-        return recurse(nums,0,dp);
-    }
-    public int recurse(int nums[],int index,int dp[]){
-        int n=nums.length;
-        if(index>=n){
-            return 0;
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[1],nums[0]);
+
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(dp[i-1],nums[i]+dp[i-2]);
         }
-        if(dp[index]!=-1) return dp[index];
-
-        int step1=nums[index]+recurse(nums,index+2,dp);
-        int step2=recurse(nums,index+1,dp);
-
-        return dp[index]=Math.max(step1,step2);
+        return dp[n-1];
     }
 }
