78a86a6d718081750b5901b2be878f325cb6a349
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-07-16 16:53:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (75.05%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,17 +1,20 @@
 class Solution {
-    int dp[]=new int[101];
     public int rob(int[] nums) {
-        Arrays.fill(dp,-1);
-        return recurse(nums,0);
-    }
-    public int recurse(int nums[],int idx){
         int n=nums.length;
-        if(idx>=n){
+        int dp[]=new int[n];
+        Arrays.fill(dp,-1);
+        return recurse(nums,0,dp);
+    }
+    public int recurse(int nums[],int index,int dp[]){
+        int n=nums.length;
+        if(index>=n){
             return 0;
         }
-        if(dp[idx]!=-1) return dp[idx];
-        int pick=nums[idx]+recurse(nums,idx+2);
-        int notake=recurse(nums,idx+1);
-        return dp[idx]=Math.max(pick,notake);
+        if(dp[index]!=-1) return dp[index];
+
+        int step1=nums[index]+recurse(nums,index+2,dp);
+        int step2=recurse(nums,index+1,dp);
+
+        return dp[index]=Math.max(step1,step2);
     }
 }
