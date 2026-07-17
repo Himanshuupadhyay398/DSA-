f1ba234695fb46f662618fde4bfd7b6ad18505b4
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-07-17 15:40:30 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (8.15%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -1,24 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
-
         if(n==1) return nums[0];
-        int dp1[]=new int[n+1];
+        int dp1[]=new int[n];
         Arrays.fill(dp1,-1);
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        int max=recurse(nums,dp,0,n-2);
-        int max1=recurse(nums,dp1,1,n-1);
-        return Math.max(max,max1);
+        int val=recurse(0,nums,n-2,dp1);
+        int dp2[]=new int[n];
+        Arrays.fill(dp2,-1);
+        int val2=recurse(1,nums,n-1,dp2);
+        return Math.max(val,val2);
     }
-    public int recurse(int nums[],int dp[],int idx,int end){
-        int n=nums.length;
-        if(idx>end){
+
+    public int recurse(int index,int nums[],int end,int dp[]){
+        if(index>end){
             return 0;
         }
-        if(dp[idx]!=-1) return dp[idx];
-        int take=nums[idx]+recurse(nums,dp,idx+2,end);
-        int notake=recurse(nums,dp,idx+1,end);
-        return dp[idx]=Math.max(take,notake);
+        if(dp[index]!=-1) return  dp[index];
+        int rob=nums[index]+recurse(index+2,nums,end,dp);
+        int skip=recurse(index+1,nums,end,dp);
+        return dp[index]=Math.max(rob,skip);
     }
 }
