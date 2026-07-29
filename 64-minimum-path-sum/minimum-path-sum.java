2286e4609bfb53a708e1a13a1d28cdffe95f0a89
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-07-29 14:52:05 +0530

    Time: 4 ms (63.03%) | Memory: 50.6 MB (10.46%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -10,32 +10,36 @@ class Solution {
     //     return totalpathMin(dp,grid,n-1,m-1);
     // }
     // public int totalpathMin(int dp[][],int grid[][],int i,int j){
+
     //     if(i<0 || j<0) return Integer.MAX_VALUE;
+
     //     if(i==0 && j==0) return grid[i][j];
+
     //     if(dp[i][j]!=-1) return dp[i][j];
+
     //     return dp[i][j]=grid[i][j] + Math.min(totalpathMin(dp,grid,i-1,j),totalpathMin(dp,grid,i,j-1));
     // }
     int n=grid.length;
     int m=grid[0].length;
     int dp[][]=new int[n][m];
 
-    for(int i=n-1;i>=0;i--){
-        for(int j=m-1;j>=0;j--){
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
 
-            if(i==n-1 && j==m-1){
+            if(i==0 && j==0){
                 dp[i][j]=grid[i][j];
             }
-            else if(i==n-1){
-                dp[i][j]=grid[i][j]+dp[i][j+1];
+            else if(i==0){
+                dp[i][j]=grid[i][j]+dp[i][j-1];
             }
-            else if(j==m-1){
-                dp[i][j]=grid[i][j]+dp[i+1][j];
+            else if(j==0){
+                dp[i][j]=grid[i][j]+dp[i-1][j];
             }
             else{
-                dp[i][j]=grid[i][j]+Math.min(dp[i+1][j],dp[i][j+1]);
+                dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);
             }
         }
     }
-    return dp[0][0];
+    return dp[n-1][m-1];
     }
 }
