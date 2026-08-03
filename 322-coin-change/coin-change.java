6aeaa4d28baa0ce13a0ce450d7827701d1e15c92
Himanshuupadhyay398 <himanshuupadhyay6604@gmail.com>
2026-08-03 16:51:01 +0530

    Time: 32 ms (17.32%) | Memory: 47.7 MB (7.88%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,13 +1,46 @@
 class Solution {
+    static final int INF = (int)1e9;
     public int coinChange(int[] coins, int amount) {
-    int dp[]=new int[amount+1];
-    Arrays.fill(dp,amount+1);
-    dp[0]=0;
-    for(int coin:coins){
-        for(int i=coin;i<dp.length;i++){
-            dp[i]=Math.min(dp[i],dp[i-coin]+1); 
-            }
+        int n=coins.length;
+        int dp[][]=new int[n][amount+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
         }
-    return dp[amount]>amount?-1:dp[amount];
+        
+        int ans= recurse(0,coins,amount,dp);
+        return ans==INF?-1:ans; 
+    }
+    public int recurse(int index,int coins[],int amount,int dp[][]){
+        int n=coins.length;
+
+        if(index>=n){
+            return amount==0?0:INF;
+        }
+
+        if(dp[index][amount]!=-1) return dp[index][amount];
+
+        int pick=INF;
+        if(amount-coins[index]>=0){
+            pick=1+recurse(index,coins,amount-coins[index],dp);
+        }
+
+        int nopick=recurse(index+1,coins,amount,dp);
+
+        return dp[index][amount]=Math.min(pick,nopick);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
